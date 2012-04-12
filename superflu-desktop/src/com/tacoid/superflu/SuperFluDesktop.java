24e5b4b271797c4e18599c43dfa2efe37e44bc3c
Maxime Chéramy <maxime81@gmail.com>
2012-04-12 19:50:21 +0200

    Initial commit.

---
+++ superflu-desktop/src/com/tacoid/superflu/SuperFluDesktop.java
@@ -0,0 +1,11 @@
+package com.tacoid.superflu;
+import com.badlogic.gdx.backends.jogl.JoglApplication;
+import com.tacoid.superflu.SuperFlu;
+
+public class SuperFluDesktop {
+	public static void main(String[] argv) {
+		new JoglApplication(new SuperFlu(), "SuperFlu", 800, 480, false);
+		//new JoglApplication(new SuperFlu(), "SuperFlu", 1024, 610, false);
+
+	}
+}

0cb386e1f505d20e5489ed0492109dc52602b033
Maxime Chéramy <maxime81@gmail.com>
2012-04-14 01:20:56 +0200

    Tout dans un Stage car c'est plus simple et ça marche. L'avion ne bouge plus.

--- superflu-android/src/com/tacoid/superflu/SuperFlu.java
+++ superflu-android/src/com/tacoid/superflu/SuperFlu.java
@@ -3,58 +3,44 @@ package com.tacoid.superflu;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class SuperFlu implements ApplicationListener, InputProcessor {
 	private static final int VIRTUAL_WIDTH = 1024;
 	private static final int VIRTUAL_HEIGHT = 544; // XXX: Très plat :);
-	private static final float ASPECT_RATIO = (float) VIRTUAL_WIDTH
-			/ (float) VIRTUAL_HEIGHT;
-	private Camera camera;
 
-	private SpriteBatch spriteBatch;
 	private Texture backgroundTexture;
 	private TextureRegion villeTextureRegion;
 	private TextureRegion backgroundTextureRegion;
-	private Sprite spriteAvion;
 	private Stage stage;
 	private int x = 0;
-	private int y = 0;
+	private int y = 520;
 
 	@Override
 	public void create() {
-		camera = new OrthographicCamera(VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
-		
-		stage = new Stage(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, true);
-		villeTextureRegion = new TextureRegion(new Texture(Gdx.files.internal("data/ville.png")), 24, 24);
-		Image img = new Image(villeTextureRegion);
-		img.x = 100;
-		img.y = 100;
-		img.width = 24;
-		img.height = 24;
-		img.originX = 0;
-		img.originY = 0;
-		stage.addActor(img);
-
-		Gdx.input.setInputProcessor(this);
-
-		spriteBatch = new SpriteBatch();
+		stage = new Stage(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false);
+	
 		backgroundTexture = new Texture(
 				Gdx.files.internal("data/fond_carte.png"));
 		backgroundTextureRegion = new TextureRegion(backgroundTexture, 1024,
 				544);
+		stage.addActor(new Image(backgroundTextureRegion));
+		
+		villeTextureRegion = new TextureRegion(new Texture(Gdx.files.internal("data/ville.png")), 24, 24);
+		Image img = new Image(villeTextureRegion);
+		img.x = 1000;
+		img.y = 520;
+		stage.addActor(img);
 
+		Gdx.input.setInputProcessor(this);
+		
 		Texture textureAvion = new Texture(Gdx.files.internal("data/avion.png"));
-		spriteAvion = new Sprite(new TextureRegion(textureAvion, 48, 48));
+		stage.addActor(new Image(new TextureRegion(textureAvion, 48, 48))); //XXX: En fait un vrai actor et pas juste une image !
+		//XXX: D'ailleurs une question : Est-ce qu'on fait de l'entity ville un actor ou est-ce qu'on fait une classe VilleActor qui contient une référence vers l'entity associée ?
 	}
 
 	@Override
@@ -64,55 +50,21 @@ public class SuperFlu implements ApplicationListener, InputProcessor {
 
 	@Override
 	public void resize(int width, int height) {
-		// calculate new viewport
-		float aspectRatio = (float) width / (float) height;
-		float scale = 1f;
-		Vector2 crop = new Vector2(0f, 0f);
+		stage.setViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, false);
+		stage.getCamera().position.set(VIRTUAL_WIDTH/2, VIRTUAL_HEIGHT/2, 0);
 
-		if (aspectRatio > ASPECT_RATIO) {
-			scale = (float) height / (float) VIRTUAL_HEIGHT;
-			crop.x = (width - VIRTUAL_WIDTH * scale) / 2f;
-		} else if (aspectRatio < ASPECT_RATIO) {
-			scale = (float) width / (float) VIRTUAL_WIDTH;
-			crop.y = (height - VIRTUAL_HEIGHT * scale) / 2f;
-		} else {
-			scale = (float) width / (float) VIRTUAL_WIDTH;
-		}
-
-		float w = (float) VIRTUAL_WIDTH * scale;
-		float h = (float) VIRTUAL_HEIGHT * scale;
-
-		// set viewport
-		Gdx.gl.glViewport((int) crop.x, (int) crop.y, (int) w, (int) h);
-	}
-
-	public void renderBackground() {
-		spriteBatch.begin();
-		spriteBatch.disableBlending();
-		spriteBatch.draw(backgroundTextureRegion, 0, VIRTUAL_HEIGHT - 544);
-		spriteBatch.end();
 	}
 
 	@Override
 	public void render() {
+		GL10 gl = Gdx.graphics.getGL10();
+
 		// Update model
-		x = (x + 1) % 500;
-
-		// ----------
-
-		// update camera
-		camera.update();
-		camera.apply(Gdx.gl10);
+		x = (x + 1) % VIRTUAL_WIDTH;
 
 		// clear previous frame
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
-		renderBackground();
-		spriteBatch.begin();
-		spriteBatch.enableBlending();
-		spriteBatch.draw(spriteAvion, x, y);
-		spriteBatch.end();
-		
 		stage.draw();
 	}
 
--- superflu-desktop/src/com/tacoid/superflu/SuperFluDesktop.java
+++ superflu-desktop/src/com/tacoid/superflu/SuperFluDesktop.java
@@ -4,8 +4,8 @@ import com.tacoid.superflu.SuperFlu;
 
 public class SuperFluDesktop {
 	public static void main(String[] argv) {
-		new JoglApplication(new SuperFlu(), "SuperFlu", 800, 480, false);
-		//new JoglApplication(new SuperFlu(), "SuperFlu", 1024, 610, false);
+		//new JoglApplication(new SuperFlu(), "SuperFlu", 800, 480, false);
+		new JoglApplication(new SuperFlu(), "SuperFlu", 1024, 600, false);
 
 	}
 }

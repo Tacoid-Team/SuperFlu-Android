8eca5b54876c1c96d103d71a796d2cf2e160114b
Maxime Chéramy <maxime81@gmail.com>
2012-04-16 21:09:08 +0200

    Ajout affichage pourcentage contamination / ville.

--- superflu-android/src/com/tacoid/actors/VilleActor.java
+++ superflu-android/src/com/tacoid/actors/VilleActor.java
@@ -1,9 +1,11 @@
 package com.tacoid.actors;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ImmediateModeRenderer10;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.tacoid.superflu.entities.Ville;
 
@@ -11,6 +13,7 @@ public class VilleActor extends Actor {
 
 	private TextureRegion villeTextureRegion;
 	private Ville ville;
+	private ImmediateModeRenderer10 renderer;
 
 	public VilleActor(Ville ville) {
 		this.ville = ville;
@@ -19,16 +22,45 @@ public class VilleActor extends Actor {
 			this.width = 25;
 			this.height = 20;
 		} else {
-			villeTextureRegion = new TextureRegion(new Texture(Gdx.files.internal("data/ville.png")), 24, 24);
-			this.width = 24;
-			this.height = 24;
+			villeTextureRegion = new TextureRegion(new Texture(Gdx.files.internal("data/ville.png")), 20, 20);
+			this.width = 20;
+			this.height = 20;
 		}
 		this.x = ville.getX();
 		this.y = 544 - ville.getY();
+		
+		renderer = new ImmediateModeRenderer10();
 	}
 	
 	@Override
 	public void draw(SpriteBatch batch, float parentAlpha) {
+		Gdx.gl11.glDisable(GL10.GL_TEXTURE_2D);
+		Gdx.gl11.glPushMatrix();
+		Gdx.gl11.glTranslatef(x, y, 0);
+
+		// On dessine la barre de contamination
+		Gdx.gl11.glColor4f(0, 0, 0, 1);
+		Gdx.gl11.glLineWidth(6);
+		renderer.begin(GL10.GL_LINES);
+		{
+			renderer.vertex( -width/2 - 1, -height/2 - 3, 0);
+			renderer.vertex( width/2 + 1, -height/2 - 3, 0);
+		}
+		renderer.end();
+		
+		float p = (float)ville.getPourcentageInfectes()/100.0f;
+		Gdx.gl11.glColor4f(p, 1-p, 0, 1);
+		Gdx.gl11.glLineWidth(4);
+		renderer.begin(GL10.GL_LINES);
+		{
+			renderer.vertex( -width/2, -height/2 - 3, 0);
+			renderer.vertex( -width/2 + p*width , -height/2 - 3, 0);
+		}
+		renderer.end();
+		
+		
+		Gdx.gl11.glPopMatrix();
+		Gdx.gl11.glEnable(GL10.GL_TEXTURE_2D);
 		batch.draw(villeTextureRegion, x - width / 2, y - height/2);
 	}
 

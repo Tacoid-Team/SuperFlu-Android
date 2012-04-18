9c69282dd95c120608996c69d1e173430f0376e2
Addict7 <nicolasfloquet@gmail.com>
2012-04-18 21:44:51 +0200

    Effet sur la ligne de création de transfert

--- superflu-android/src/com/tacoid/superflu/actors/TransitionCreator.java
+++ superflu-android/src/com/tacoid/superflu/actors/TransitionCreator.java
@@ -4,7 +4,11 @@ import android.opengl.GLSurfaceView.Renderer;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.GL11;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureWrap;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ImmediateModeRenderer10;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
@@ -18,7 +22,12 @@ public class TransitionCreator extends Actor {
 	private float end_x = 0;
 	private float end_y = 0;
 	
-	private boolean enabled = true;
+	private boolean enabled = false;
+	
+	ImmediateModeRenderer10 renderer;
+	
+	Texture arrow;
+	float tex_offset = 0.0f;
 
 	public static TransitionCreator getInstance() {
         if (null == instance) {
@@ -32,23 +41,42 @@ public class TransitionCreator extends Actor {
 		this.y = 0;
 		this.width = Gdx.graphics.getWidth();
 		this.height = Gdx.graphics.getHeight();
+		renderer =  new ImmediateModeRenderer10();
+		arrow = new Texture(Gdx.files.internal("images/transfert_arrow.png"));
     }
 	
 	@Override
 	public void draw(SpriteBatch arg0, float arg1) {
+		float norm = (float) Math.sqrt((end_x-start_x)*(end_x-start_x)+(end_y-start_y)*(end_y-start_y));
+		norm = norm/4.0f;
+		float ortho_x = (end_y-start_y)/norm;
+		float ortho_y = -(end_x-start_x)/norm;
 		if(enabled) {
-			ImmediateModeRenderer10 renderer =  new ImmediateModeRenderer10();
-			Gdx.gl11.glDisable(GL10.GL_TEXTURE_2D);
-	        Gdx.gl11.glPushMatrix();
+			Gdx.gl11.glPushMatrix();
 	
 	        Gdx.gl11.glColor4f(1.0f, 1.0f, 1.0f, 1);
+	        
 	        Gdx.gl11.glLineWidth(3);
-	        renderer.begin(GL10.GL_LINES);
+	        
+	        Gdx.gl11.glEnable(GL11.GL_TEXTURE_2D);
+	        arrow.setWrap( TextureWrap.Repeat, TextureWrap.Repeat );
+	        arrow.bind();
+	        
+	        renderer.begin(GL11.GL_TRIANGLE_STRIP);
 	        {
-	                renderer.vertex( start_x, start_y,0);
-	                renderer.vertex( end_x, end_y, 0);
+	        		renderer.texCoord(0, -tex_offset);
+	                renderer.vertex( start_x-ortho_x, start_y-ortho_y,0);
+	                renderer.texCoord(0, norm-tex_offset);
+	                renderer.vertex( end_x-ortho_x, end_y-ortho_y, 0);
+	                renderer.texCoord(1, -tex_offset);
+	                renderer.vertex( start_x+ortho_x, start_y+ortho_y,0);
+	                renderer.texCoord(1,norm-tex_offset);
+	                renderer.vertex( end_x+ortho_x, end_y+ortho_y, 0);
 	        }
 	        renderer.end();
+	        tex_offset += 0.2f;
+	        if(tex_offset >= 16.0f)
+	        	tex_offset = 0.0f;
 	        
 	        Gdx.gl11.glPopMatrix();
 	        Gdx.gl11.glEnable(GL10.GL_TEXTURE_2D);
@@ -62,7 +90,6 @@ public class TransitionCreator extends Actor {
 
 	public void enable(float x, float y) {
 		enabled = true;
-		System.out.println(x + "," + y);
 		start_x = x;
 		start_y = y;
 		end_x = x;
